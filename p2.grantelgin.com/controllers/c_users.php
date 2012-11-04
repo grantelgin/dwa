580129eec511768e2724b7f67d2b4960680ed547
grantElgin <grant@grantelgin.com>
2012-11-03 22:35:57 -0400

    trying to get around DB138 error by redirecting to profile

--- p2.grantelgin.com/controllers/c_users.php
+++ p2.grantelgin.com/controllers/c_users.php
@@ -99,7 +99,7 @@ class users_controller extends base_controller {
 			 setcookie("token", $token, strtotime('+1 year'), '/');
 		
 			 # Send them to the main page - or whever you want them to go
-			 Router::redirect("/posts");			
+			 Router::redirect("/users/profile");			
 		}
 	}
 
