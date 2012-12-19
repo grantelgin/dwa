8aefc8e2ca4c66568b024145eaebee477c20638b
grantElgin <grant@grantelgin.com>
2012-12-19 16:18:45 -0500

    style updates

--- p4.grantelgin.com/views/v_regulators_items.php
+++ p4.grantelgin.com/views/v_regulators_items.php
@@ -1,4 +1,4 @@
-<div class="title" style="padding-top:30px; padding-bottom:10px; text-align:center;">Regulators</div>
+<div class="title" style="padding-top:30px; padding-bottom:10px; text-align:center; width:600px;">Regulators</div>
 <form method='POST' action='/posts/p_follow'>
 		
 			
@@ -6,27 +6,29 @@
 		<? foreach($complianceItems as $ci): ?>
 		<div class="post" style="width:600px;">
 		<!-- Print this user's name -->
-		<div style="float:left;">
+<div style="float:left; margin-top:20px;">
 		<img src="../images/<?=$ci['regulatorLogo']?>.png" alt="logo_1" width="60" />
 </div>
-<div style="float:left;">
-<p>
-	<?=$ci['ciName']?><br/>
-	<?=$ci['ciDesc']?>
-</p>
-</div>
-<div style="float:right;">
-		<p><a href="/regulators/profile/<?=$ci['regulator_id']?>"><?=$ci['regulatorName']?></a><br/>  
-			<span class="trade"><?=$ci['regulatorWww']?></span></span>
-		</p>
-		<p>
-			<?=$ci['regulatorPhone']?><br/>
-			<?=$ci['regulatorAddDesc']?><br/>
-			<?=$ci['regulatorStreet1']?><br/>
-			<?=$ci['regulatorStreet2']?><br/>
-		</p>
-		
-</div>
+
+	<p style="padding-left:70px;">
+		<?=$ci['ciName']?>
+	</p>
+	<p style="margin-left:70px;">
+		<span style="float:left;"><a href="/regulators/profile/<?=$ci['regulator_id']?>"><?=$ci['regulatorName']?></a></span><span style="float:right;">Add reminder</span>
+	</p>
+
+<!-- <div style="float:right;">											    -->
+<!-- 		<p><br/>  													    -->
+<!-- 			<span class="trade"><?=$ci['regulatorWww']?></span></span>  -->
+<!-- 		</p>														    -->
+<!-- 		<p>															    -->
+<!-- 			<?=$ci['regulatorPhone']?><br/>							    -->
+<!-- 			<?=$ci['regulatorAddDesc']?><br/>						    -->
+<!-- 			<?=$ci['regulatorStreet1']?><br/>						    -->
+<!-- 			<?=$ci['regulatorStreet2']?><br/>						    -->
+<!-- 		</p>														    -->
+<!-- 		<?=$ci['ciDesc']?>											    -->
+<!-- </div>																    -->
 <div style="clear:both;">&nbsp;</div>
 		
 		<!-- If there exists a connection with this user, show a unfollow link -->
